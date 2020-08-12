bb14b61c39f51c91cdf0b8dce185d58680c1f6bd
Peter Opiyo <peteropiyo@gmail.com>
2020-08-12 16:17:02 +0300

    created the calculator framework

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+
+alert(add(number1, number2));
+var number1 = parseInt(prompt("Enter a Number"));
+var number2 = parseInt(prompt("Enter a 2nd Number"));
