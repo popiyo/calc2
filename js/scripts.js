b1e26e247999ce0ad53be68b802fa28440f637ac
Peter Opiyo <peteropiyo@gmail.com>
2020-08-28 00:11:50 +0300

    final calculator code

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,21 @@ var add = function(number1, number2) {
   return number1 + number2;
 };
 
-alert(add(number1, number2));
+var subtract = function(number1, number2) {
+  return number1 - number2;
+};
+
+var multiply = function(number1, number2) {
+  return number1 * number2;
+};
+
+var divide = function(number1, number2) {
+  if(number2!==0){  return number1 / number2;}
+  else return 0;
+};
+
+
 var number1 = parseInt(prompt("Enter a Number"));
 var number2 = parseInt(prompt("Enter a 2nd Number"));
+
+alert(divide(number1, number2));
